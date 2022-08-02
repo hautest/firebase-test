32c6a6ad6656d4f4ec93623b0de31511b5066eee
LeeSeungHun <dos07008@naver.com>
2022-08-02 19:32:45 +0900

    chore : setting

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBu2rDVRiyhsugpLWAsrDWrhoVZmiM4WEo",
+  authDomain: "test-firebase-d594b.firebaseapp.com",
+  projectId: "test-firebase-d594b",
+  storageBucket: "test-firebase-d594b.appspot.com",
+  messagingSenderId: "379321634435",
+  appId: "1:379321634435:web:73a77c40667bbea26095b6",
+  measurementId: "G-TYS9RJV38Q",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+export const authService = firebase.auth();

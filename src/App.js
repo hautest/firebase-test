3f61ee51866154dee0774711f6ea73a09857398c
LeeSeungHun <dos07008@naver.com>
2022-08-02 19:40:38 +0900

    Home, Auth페이지 라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Auth } from "./pages/Auth";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
